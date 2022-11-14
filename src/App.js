f9709d7bb55214cc96ab344a37d30949ad04f27d
kashifx6 <mkashifx6@gmail.com>
2022-11-14 11:32:53 +0500

    Covid 19 Tracker

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Covid from "./components/Covid";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <Covid />
+    </div>
+  );
+}
+
+export default App;
